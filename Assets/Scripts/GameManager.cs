9aef36d8d4ddd120d25f93d8704bb3b4d6720c66
Ed <eduardas.lapinskas@gmail.com>
2023-07-14 15:19:13 +0300

    gamemanager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int score;
+    public TextMeshProUGUI scoreText;
+    public GameObject gameoverScreen;
+
+    [SerializeField] TextMeshProUGUI highScoreText;
+
+    public void addScore(int scoreToAdd)
     {
-        
+        score += scoreToAdd;
+        scoreText.text = score.ToString();
+        CheckHighscore();
     }
 
-    // Update is called once per frame
-    void Update()
+    void CheckHighscore()
     {
-        
+        if (score > PlayerPrefs.GetInt("Highscore", 0))
+            PlayerPrefs.SetInt("Highscore", score);
+    }
+
+    void UpdateHighScoreText()
+    {
+        highScoreText.text = PlayerPrefs.GetInt("Highscore", 0).ToString();
+    }
+
+    public void gameOver()
+    {
+        UpdateHighScoreText();
+        gameoverScreen.SetActive(true);
+    }
+
+
+    //Pressed button script
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
